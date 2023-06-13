9269c00ee2f51e2bd21212975213285e58fa4bc3
Francogomez23800 <francogomez23800@gmail.com>
2023-06-13 16:39:04 -0300

    Responsive navbar done

---
+++ src/Components/index.js
@@ -0,0 +1,29 @@
+import Navbar from './Navbar'
+import Hero from './Hero'
+import Billing from './Billing'
+import Business from './Business'
+import CardDeal from './CardDeal'
+import Testimonials from './Testimonials'
+import Button from './Button'
+import FeedbackCard from './FeedbackCard'
+import Stats from './Stats'
+import CTA from './CTA'
+import GetStarted from './GetStarted'
+import Footer from './Footer'
+import Clients from './Clients'
+
+export{
+    Navbar,
+    Hero,
+    Billing,
+    Business,
+    CardDeal,
+    Testimonials,
+    Button,
+    FeedbackCard,
+    Stats,
+    CTA,
+    GetStarted,
+    Footer,
+    Clients
+}
