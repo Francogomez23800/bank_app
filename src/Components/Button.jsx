549a2bcf5b6eb2520ff318c292baf5bb5385f751
Francogomez23800 <francogomez23800@gmail.com>
2023-06-14 14:57:12 -0300

    Business section done.

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import styles from '../styles'
 
-function Button() {
-  return (
-    <div>Button</div>
+const Button = ({styles}) => (
+    <button 
+    type='button' 
+    className={`py-4 px-6 bg-blue-gradient font-poppins
+     font-medium text-[18px] text-primary outline-none ${styles} rounded-lg`}>
+      Get Started
+     </button>
   )
-}
 
 export default Button
--- src/Components/GetStarted.jsx
+++ src/Components/GetStarted.jsx
@@ -2,7 +2,7 @@ import styles from '../styles'
 import { arrowUp } from '../assets'
 
 const GetStarted = () =>(
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient cursor-pointer`}>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full border-[2px] bg-blue-gradient cursor-pointer`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
           <p className='font-poppins font-medium text-[18px] leading-[23x] mr-2'>
